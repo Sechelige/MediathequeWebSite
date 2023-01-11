d97c952a1c99cfb11a68345915f5c80c30b70b75
Sechelige <toinou.deurv@gmail.com>
2023-01-12 00:13:27 +0100

    Update controleurAcceuil.php

--- controleur/controleurAcceuil.php
+++ controleur/controleurAcceuil.php
@@ -7,7 +7,7 @@ class ControleurAcceuil {
         include ("vue/navBar.html");
         echo "<h1>Acceuil</h1>";
         echo "<a href='index.php?controleur=controleurUtilisateur&numUtilisateur=1'><h2>Go to InformationPerso</h2> </a>";
-        //include ("vue/acceuil.html");
+        //include ("vue/acceuil.html");//TODO
         include ("vue/footer.html");
     }
 }

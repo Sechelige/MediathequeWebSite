f555dd6f4c819b833d6164934949911e7b5e5589
Sechelige <toinou.deurv@gmail.com>
2023-01-14 10:26:12 +0100

    update reservation exemplaire

--- controleur/controleurExemplaire.php
+++ controleur/controleurExemplaire.php
@@ -6,17 +6,16 @@ class ControleurExemplaire {
 
     public static function afficheExemplaireOuvrage ($ouv) {
         $tab_exemplaire = Exemplaire::getExemplaireByNumOuvrage($ouv);
-        var_dump($tab_exemplaire->get("estReserveExemplaire"));
-        /*if ($tab_exemplaire != false) {
-            if ($tab_exemplaire->get("estReserveExemplaire") == 0 && $tab_exemplaire->get("empruntExemplaire") == 0){
+        if ($tab_exemplaire != false) {
                 echo "<div class='ex-dispo-titre'>Exemplaires disponibles pour cet ouvrage: <button onclick=''>+</button></div>";
                 foreach ($tab_exemplaire as $exemplaire) {
-                    include "vue/exemplaire/afficheExemplaire.html";
-                }
+                    if ($exemplaire->get("estReserveExemplaire") == 0 && $exemplaire->get("empruntExemplaire") == 0){
+                        include "vue/exemplaire/afficheExemplaire.html";
+                    }
             }
         } else {
             echo "<div class='ex-dispo-titre'>Exemplaires réservés/empruntés (non disponibles) : </div>";
             echo "Aucun exemplaire trouvé pour cet ouvrage";
-        }*/
+        }
     }
 }

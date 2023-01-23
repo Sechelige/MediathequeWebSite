98d85ebfae5d6728f2e9854157569ead3ee5c08b
Subjectory <ineozz_@outlook.fr>
2023-01-23 21:12:22 +0100

    coco

--- controleur/controleurConnexion.php
+++ controleur/controleurConnexion.php
@@ -6,6 +6,28 @@ class ControleurConnexion {
         $titre = "Connexion";
         include ("vue/debut.php");
         include ("vue/header-one/header.php");
-        include ("vue/connexion/connexion.php");
+        include ("vue/connexion/connexion.html");
+        include ("vue/footer/footer.html");
+    }
+
+    public static function connecterUtilisateur(){
+        $login = $_POST["login"];
+        $mdp = $_POST["mdp"];
+
+        $b = Utilisateur::checkMDP($login, $mdp);
+
+        if($b){
+            $_SESSION["login"] = $_POST["login"];
+            ControleurAcceuil::afficheAcceuil();
+        } else{
+            self::afficheConnexion();
+        }
+    }
+
+    public static function deconnecterUtilisateur(){
+        session_unset();
+        session_destroy();
+        setcookie(session_name(), '', time()-1);
+        self::afficheConnexion();
     }
 }

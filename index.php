c0d763a1e7479e9e7d975c12a84bdb0070a2a398
Subjectory <ineozz_@outlook.fr>
2023-01-24 14:42:58 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
   session_start();
   // affichage de contrôle du tableau $_SESSION
-  // echo "<pre>session courante : <br>"; print_r($_SESSION); echo "</pre>";
+   echo "<pre>session courante : <br>"; print_r($_SESSION); echo "</pre>";
   require_once("./controleur/router.php");
   
 ?>

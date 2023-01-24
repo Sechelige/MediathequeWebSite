98672f319459c990de3a7e7337db2b839adf2fa1
Sechelige <toinou.deurv@gmail.com>
2023-01-24 22:29:48 +0100

    accueil

--- controleur/controleurAcceuil.php
+++ controleur/controleurAcceuil.php
@@ -6,7 +6,7 @@ class ControleurAcceuil {
         $titre = "Accueil";
         include ("vue/debut.php");
         include ("vue/header-one/header.php");
+        echo "<a href='index.php?controleur=controleurConnexion'>Connexion </a>";
         include ("vue/acceuil/acceuil.html");//TODO
-        include ("vue/footer/footer.html");
     }
 }
